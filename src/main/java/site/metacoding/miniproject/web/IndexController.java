67bf2e394079ae069725a51e1f0c305cb32aa76d
jinsa <jinsa004@gmail.com>
2022-10-11 21:19:14 +0900

    기초세팅 완료

---
+++ src/main/java/site/metacoding/miniproject/web/IndexController.java
@@ -0,0 +1,12 @@
+package site.metacoding.miniproject.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String main() {
+        return "index";
+    }
+}
