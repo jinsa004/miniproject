38e63449e9f3b091f769722b58c55c2a76dd5a38
bigjang77 <dhex2004@gmail.com>
2022-10-12 21:06:30 +0900

    Mapper, 엔티티 작성완료

--- src/main/java/site/metacoding/miniproject/domain/resume/Resume.java
+++ src/main/java/site/metacoding/miniproject/domain/resume/Resume.java
@@ -29,4 +29,11 @@ public class Resume {
     private Integer jobId;
     private boolean isMain;
     private Timestamp createdAt;
+
+    private String employeeName;
+    private String employeeBrith;
+    private String employeeSex;
+    private String employeeEmail;
+    private String employeeTel;
+    private String employeeLocation;
 }
