a56b35213b9236479c17ea252abc054ceeab3772
jinsa <jinsa004@gmail.com>
2022-10-12 21:48:00 +0900

    내 공고 목록보기 세션값으로 띄우기 header수정 완료 기능구현 완료

--- src/main/java/site/metacoding/miniproject/web/NoticeController.java
+++ src/main/java/site/metacoding/miniproject/web/NoticeController.java
@@ -2,8 +2,6 @@ package site.metacoding.miniproject.web;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
