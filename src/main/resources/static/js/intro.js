42fdc458c94a79366e10bdcae6415fc1d2f3cd17
hyonna12 <hyonna01@gmail.com>
2022-10-19 15:13:51 +0900

    웹소켓 구독 및 컨택하기 완료

--- src/main/resources/static/js/intro.js
+++ src/main/resources/static/js/intro.js
@@ -35,7 +35,7 @@ function insertSub() {
       renderSub();
       $("#subscribeId").val(res.data.introId);
       console.log(res);
-      //location.reload();
+      location.reload();
     } else {
       alert("구독을 실패했습니다");
     }
